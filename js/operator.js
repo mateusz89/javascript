bc1c03e61a7109de7f264d4ee1907db33124fdc4
Mateusz Wójkowski <mat.wojkowski@gmail.com>
2017-08-19 15:30:14 +0200

    kolejny commit

---
+++ js/operator.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var liczba1 = 13, liczba2 = 5;
+
+var wynik;
+
+wynik = liczba1 % liczba2;
+
+console.log("krok 1: " + wynik);
+
+console.log("krok 2: " + ++wynik);
+
+console.log("krok 3: " + wynik);
+
+liczba1 += 8;
+
+var tekst = "Wojtek";
+
+tekst += "Potocki";
+
+console.log(tekst);
+
+console.log("***********************************");
+
+var liczba = 10;
+var tekst = "10";
+
+//console.log(liczba == tekst);
+
+//console.log(13 == 5 || 10 == "10");
+
+(2>5) ? console.log("tak") : console.log("nie");
