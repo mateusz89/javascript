3b8972b0bbfe602aea546fde2b4fbf45de5516e7
Mateusz Wójkowski <mat.wojkowski@gmail.com>
2017-08-19 14:33:59 +0200

    przyklady2

--- js/objects.js
+++ js/objects.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var osoba = {
+/*var wojtek = {
     imie: "Wojtek", 
     wzrost: 181,
     przedstawOsobe: function() {
@@ -8,12 +8,61 @@ var osoba = {
     }
 }
 
-console.log(osoba.imie);
-console.log(osoba.wzrost);
+var kaja = {
+    imie: "Kaja", 
+    wzrost: 160,
+    przedstawOsobe: function() {
+        console.log("Cześć jestem " + this.imie + ", mam " + this.wzrost + " wzrostu. ");
+    }
+}
 
-osoba.imie = "Adam";
 
-console.log(osoba["imie"]);
-console.log(osoba["wzrost"]);
+console.log(wojtek.imie);
+console.log(wojtek.wzrost);
 
-osoba.przedstawOsobe();
+
+console.log(wojtek["imie"]);
+console.log(wojtek["wzrost"]);
+
+wojtek.przedstawOsobe();
+
+console.log(kaja.imie);
+console.log(kaja.wzrost);
+
+
+console.log(kaja["imie"]);
+console.log(kaja["wzrost"]);
+
+kaja.przedstawOsobe();*/
+
+class Osoba {
+    constructor(imie, nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wzrost;
+        this.oczy;
+    }
+
+    wyswietlInfo() {
+        console.log("Imię: " 
+                    + this.imie 
+                    + "\n" 
+                    + "Nazwisko: " 
+                    + this.nazwisko 
+                    + "\n" + "Wzrost: " 
+                    + this.wzrost 
+                    + "\n" 
+                    + "Oczy: " 
+                    + this.oczy);
+    }
+}
+
+var wojtek = new Osoba("Wojtek", "Potocki");
+
+wojtek.wzrost = 180;
+wojtek.oczy = "niebieskie";
+
+var Karol = new Osoba("Karol", "Młodawski");
+
+wojtek.wyswietlInfo();
+Karol.wyswietlInfo();
